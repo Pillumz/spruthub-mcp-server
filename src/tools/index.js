f8faab888c9285312e3e4c7f12e11e79ed12b488
root <root@5mlt.l.time4vps.cloud>
2025-12-06 12:29:31 +0200

    feat: add tools directory structure

---
+++ src/tools/index.js
@@ -0,0 +1,10 @@
+// src/tools/index.js
+export { handleListAccessories } from './listAccessories.js';
+export { handleGetAccessory } from './getAccessory.js';
+export { handleListRooms } from './listRooms.js';
+export { handleListScenarios } from './listScenarios.js';
+export { handleGetScenario } from './getScenario.js';
+export { handleGetLogs } from './getLogs.js';
+export { handleControlAccessory } from './controlAccessory.js';
+export { handleControlRoom } from './controlRoom.js';
+export { handleRunScenario } from './runScenario.js';
